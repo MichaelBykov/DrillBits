2ca11a3966e53a375f7b502b6aae68b67805d405
Michael Bykov <michaelbykov@hotmail.com>
2019-07-10 21:51:33 -0700

    Initial Commit

---
+++ DrillBits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrillBits
+//
+//  Created by Michael Bykov on 7/10/19.
+//  Copyright © 2019 Lepario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
