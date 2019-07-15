e8e013ca2c0d8350ae08f6e8045b9fc8032ecd1a
Michael Bykov <michaelbykov@hotmail.com>
2019-07-15 10:06:38 -0700

    Added the size picker UI

--- DrillBits/ViewController.swift
+++ DrillBits/ViewController.swift
@@ -9,12 +9,94 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var ScrollView: UIScrollView!
+	
+	//
+	// Size
+	//
+	
+	@IBOutlet weak var SizeWhole: UILabel!
+	@IBOutlet weak var SizeNumerator: UILabel!
+	@IBOutlet weak var SizeDenominator: UILabel!
+	// We will remove this when unit type = mm
+	@IBOutlet weak var SizeFractionWrapper: UIView!
+	@IBOutlet weak var SizeFractionLeading: NSLayoutConstraint!
+	@IBOutlet weak var SizeFractionTrailing: NSLayoutConstraint!
+	@IBOutlet weak var SizeUnitToWhole: NSLayoutConstraint!
+	@IBOutlet weak var SizeUnit: UILabel!
+	@IBOutlet weak var SizeUnitSelector: UISegmentedControl!
+	
+	public var WholeValue: CGFloat = 3;
+	public var NumeratorValue: CGFloat = 0;
+	public var DenominatorValue: CGFloat = 16;
+	
+	/// Are we using the Imperial (inches) or Metric (mm) system?
+	public var IsImperial: Bool = true;
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		// Make sure our scroll view has proper insets
+		ScrollView.contentInsetAdjustmentBehavior = .always;
 	}
-
-
+	
+	
+	
+	//
+	// Size
+	//
+	
+	/// Update the size input size value
+	public func SizeUpdateFraction() {
+		if (IsImperial) {
+			// Floor `WholeValue` so we won't have decimals in the fraction
+			SizeWhole.text			= "\(floor(WholeValue))";
+			SizeNumerator.text		= "\(NumeratorValue)";
+			SizeDenominator.text	= "\(DenominatorValue)";
+		} else {
+			SizeWhole.text			= "\(WholeValue)";
+		}
+	}
+	
+	@IBAction func SizeUnitSelectorValueChanged(_ sender: UISegmentedControl) {
+		// What the last selected value was
+		let LastIsImperial = IsImperial
+		
+		// What was selected?
+		IsImperial = sender.selectedSegmentIndex == 0;
+		
+		// Just in case
+		if (LastIsImperial == IsImperial) {
+			return;
+		}
+		
+		// Update unit text
+		SizeUnit.text = IsImperial ? "in" :
+		"mm";
+		
+		// Remove or add back fraction
+		if (IsImperial) {
+			// Reposition
+			SizeFractionWrapper.isHidden = false;
+			SizeUnitToWhole.priority = UILayoutPriority.defaultLow;
+			SizeFractionLeading.priority = UILayoutPriority.defaultHigh;
+			SizeFractionTrailing.priority = UILayoutPriority.defaultHigh;
+		} else {
+			SizeFractionWrapper.isHidden = true;
+			SizeFractionLeading.priority = UILayoutPriority.defaultLow;
+			SizeFractionTrailing.priority = UILayoutPriority.defaultLow;
+			SizeUnitToWhole.priority = UILayoutPriority.defaultHigh;
+		}
+		
+		updateViewConstraints();
+	}
+	
+	@IBAction func SizeValueChanged(_ sender: UISnappingSlider) {
+		// TODO: Add ability to choose the drill bit's size
+	}
 }
 
