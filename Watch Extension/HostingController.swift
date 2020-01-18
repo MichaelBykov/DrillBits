60e6af7683bcb79a3ac761bb7bdf19acfe45d207
Michael Bykov <michaelbykov@hotmail.com>
2020-01-17 23:10:44 -0800

    Added apple watch support

---
+++ Watch Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Watch Extension
+//
+//  Created by Michael Bykov on 1/17/20.
+//  Copyright © 2020 Lepario. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
