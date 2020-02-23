aa711ac031366fd1dca20229a4307826f6365644
Michael Bykov <michaelbykov@hotmail.com>
2020-02-23 11:37:43 -0800

    Added complications

--- Watch Extension/ExtensionDelegate.swift
+++ Watch Extension/ExtensionDelegate.swift
@@ -21,6 +21,14 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
     func applicationWillResignActive() {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
+		
+		// Tell the complication to update
+		let server = CLKComplicationServer.sharedInstance();
+		if (server.activeComplications != nil) {
+			for complication in server.activeComplications! {
+				server.reloadTimeline(for: complication);
+			}
+		}
     }
 
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
